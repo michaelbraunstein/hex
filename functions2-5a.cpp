4356e4766aa366f3f0dbdeac81e9df3b193e35db
Patrick Cronin <pcronin@maxmind.com>
2018-03-29 15:39:07 -0400

    Fix some stuff

--- functions2-5a.cpp
+++ functions2-5a.cpp
@@ -28,47 +28,41 @@ int main()
 	cin >> choice;
 
 
-	if (choice == 1)
-	{
+	if (choice == 1) {
 		int costype; // cosine, arc, hyperbolic;
 		float cosi, result;// arc, hyperbolic, result;
+
 		cout << "Type 1 if you want to find the cosine, type 2 if you want to find the arc cosine, or 3 if you wnat to find the hyperbolic cosine? \n" << endl;
 		cout << "Enter your choice\n" << endl;
 		cin >> costype;
-		if (costype = 1) {
+
+		if (costype == 1) {
 			cout << "Enter your number\n" << endl;
 			cin >> cosi;
 			result = cos(cosi);
 			cout << "The cosine of " << cosi << " is " << result << " /n" << endl;
-			system("pause");
 		}
 		else if (costype == 2) {
 			cout << "Enter your number\n" << endl;
 			cin >> cosi;
 			result = acos(cosi);
 			cout << "The arc of " << cosi << " is " << result << " /n" << endl;
-			system("pause");
-
 		}
 		else if (costype == 3) {
 			cout << "Enter your number\n" << endl;
 			cin >> cosi;
 			result = cosh(cosi);
 			cout << "The hyperbolic cosin of " << cosi << " is " << result << "\n" << endl;
-			system("pause");
-
 		}
-
+		system("pause");
 	}
 
-	else if (choice == 2)
-	{
+	else if (choice == 2) {
 		int logtype;
-
 		float cos, result;
+
 		cout << "If you want to find the common logarithm press 1, if you want to find the naturural logarithm press 2 \n" << endl;
 		cin >> logtype;
-
 
 		if (logtype == 1) {
 			cout << "Enter the number you want to find the common logarithm for: \n" << endl;
@@ -76,8 +70,6 @@ int main()
 			result = log(cos);
 
 			cout << "The common logarithm of " << cos << " is " << result << "\n " << endl;
-			system("pause");
-
 		}
 		else if (logtype == 2) {
 			cout << "Enter the number you want to find the natural logarithm for: \n";
@@ -85,13 +77,11 @@ int main()
 			result = log10(cos);
 
 			cout << "The natural logarithm of " << cos << " is " << result << " \n" << endl;
-			system("pause");
-
 		}
+		system("pause");
 	}
 
-	else if (choice == 3)
-	{
+	else if (choice == 3) {
 		int dec, rem;
 		string hrx, decout;
 		int conv;
@@ -115,7 +105,6 @@ int main()
 			decout = dec;
 			while (dec > 15)
 			{
-
 				rem = dec % 16;
 				dec = dec / 16;
 				if (rem < 10) hrx = std::to_string(rem) + hrx;
@@ -134,75 +123,56 @@ int main()
 			else if (dec == 13) hrx = "D" + hrx;
 			else if (dec == 14) hrx = "E" + hrx;
 			else if (dec == 15) hrx = "F" + hrx;
+
+			if (up == false) {
+				for (int i = 0; i < hrx.length(); i++)
+					hrx[i] = tolower(hrx[i]);
+			}
+			cout << "The hexadecimal version of " << decout << " is H0" << hrx << "\n" << endl;
+			system("pause");
 		}
-		if (up == false) {
-			for (int i = 0; i = (hrx.length(), i++);
-				hrx[i] = tolower(hrx[i]));
+
+		else if (conv == 2) {
+			int pwr, dec;
+			string myhex = "";
+			pwr = 0;
+			dec = 0;
+
+
+			cout << "Enter the hexadecimal number you wish to convert to decimal: \n" ;
+			cin >> myhex;
+
+
+			for (int i = myhex.length() - 1; i >= 0; i--) {
+				if      (myhex[i] == '0') dec += 0 * pow(16, pwr);
+				else if (myhex[i] == '1') dec += 1 * pow(16, pwr);
+				else if (myhex[i] == '2') dec += 2 * pow(16, pwr);
+				else if (myhex[i] == '3') dec += 3 * pow(16, pwr);
+				else if (myhex[i] == '4') dec += 4 * pow(16, pwr);
+				else if (myhex[i] == '5') dec += 5 * pow(16, pwr);
+				else if (myhex[i] == '6') dec += 6 * pow(16, pwr);
+				else if (myhex[i] == '7') dec += 7 * pow(16, pwr);
+				else if (myhex[i] == '8') dec += 8 * pow(16, pwr);
+				else if (myhex[i] == '9') dec += 9 * pow(16, pwr);
+				else if (myhex[i] == 'A') dec += 10 * pow(16, pwr);
+				else if (myhex[i] == 'B') dec += 11 * pow(16, pwr);
+				else if (myhex[i] == 'C') dec += 12 * pow(16, pwr);
+				else if (myhex[i] == 'D') dec += 13 * pow(16, pwr);
+				else if (myhex[i] == 'E') dec += 14 * pow(16, pwr);
+				else if (myhex[i] == 'F') dec += 15 * pow(16, pwr);
+				pwr++;
+			}
+
+			cout << "The decimal version of " << (myhex) << " is " << (dec) << "\n" << endl;
+			//system("pause");
 		}
-		cout << "The hexadecimal version of " << decout << " is H0" << hrx << "\n" << endl;
-
-		system("pause");
-
-
-
-
 	}
-
-
-
-	else if (conv == 2) {
-		int pow, dec;
-		string myhex = "";
-		pow = 0;
-		dec = 0;
-
-
-		cout << "Enter the hexadecimal number you wish to convert to decimal: \n" ;
-		cin >> myhex;
-
-
-		for (int i = myhex.length() - 1; i = 0; i--) {
-			if      (myhex[i] == '0') dec += 0 * (1 << (4 * pow));
-			else if (myhex[i] == '1') dec += 1 * (1 << (4 * pow));
-			else if (myhex[i] == '2') dec += 2 * (1 << (4 * pow));
-			else if (myhex[i] == '3') dec += 3 * (1 << (4 * pow));
-			else if (myhex[i] == '4') dec += 4 * (1 << (4 * pow));
-			else if (myhex[i] == '5') dec += 5 * (1 << (4 * pow));
-			else if (myhex[i] == '6') dec += 6 * (1 << (4 * pow));
-			else if (myhex[i] == '7') dec += 7 * (1 << (4 * pow));
-			else if (myhex[i] == '8') dec += 8 * (1 << (4 * pow));
-			else if (myhex[i] == '9') dec += 9 * (1 << (4 * pow));
-			else if (myhex[i] == 'A') dec += 10 * (1 << (4 * pow));
-			else if (myhex[i] == 'B') dec += 11 * (1 << (4 * pow));
-			else if (myhex[i] == 'C') dec += 12 * (1 << (4 * pow));
-			else if (myhex[i] == 'D') dec += 13 * (1 << (4 * pow));
-			else if (myhex[i] == 'E') dec += 14 * (1 << (4 * pow));
-			else if (myhex[i] == 'F') dec += 15 * (1 << (4 * pow));
-			pow++;
-		}
-
-
-
-
-
-
-		cout << "The decimal version of " << (myhex) << " is " << (dec) << "\n" << endl;
-		//system("pause");
-
-	}
-
-
 
 	else if (choice == 4) {
-		{
-			cout << "Exiting program\n" << endl;
+		cout << "Exiting program\n" << endl;
 
-		}
 		exit(EXIT_SUCCESS);
 	}
 
-
 	system("pause");
 }
-
-
